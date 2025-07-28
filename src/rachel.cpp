1e254e082b30776906bb495a857e709f06d4cc2d
Martin Ahrnbom <m.ahrnbom@outlook.com>
2025-07-28 08:44:33 +0200

    whoops

--- src/rachel.cpp
+++ src/rachel.cpp
@@ -26,19 +26,15 @@ namespace rachel
             return false;
         }
 
-        std::cout << "1" << std::endl;
         handle_callbacks();
 
-        std::cout << "2" << std::endl;
         const auto now = current_time();
         const auto elapsed = now - _last_loop_condition;
         const auto remaining = _time_delta - elapsed;
         _last_loop_condition = now;
         if (remaining > TimeDelta::zero()) {
-            std::cout << "3" << std::endl;
             std::this_thread::sleep_for(remaining);
         }
-        std::cout << "4" << std::endl;
         return true;
     }
 }
