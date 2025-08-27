51771b08b676dd87bfc071a3c41e260ed3967dda
Martin Ahrnbom <m.ahrnbom@outlook.com>
2025-08-27 22:33:34 +0200

    a little bit closer to what I had in mind

--- default_nodes/src/transforms_node.cpp
+++ default_nodes/src/transforms_node.cpp
@@ -44,20 +44,80 @@ struct std::hash<transforms_node::TransformKey>
 
 namespace transforms_node {
 
-std::unordered_map<TransformKey, Eigen::Isometry3d> transforms;
+using Isometry = Eigen::Isometry3d;
+
+/*
+    Represents a transform T such that if X is a point in the source frame, then Y=T*X will be the corresponding point in
+    the target frame. To transform a direction, use the `apply_to_direction` function.
+*/
+class TransformObj {
+private:
+    Isometry transform;
+    std::string source, target;
+
+public:
+    /*
+        Dummy constructor needed by `std::unordered_map`
+    */
+    TransformObj() {
+        source = target = "";
+        transform.setIdentity();
+    }
+
+    /*
+        Main constructor
+    */
+    TransformObj(const std::string& source, const std::string& target, const Isometry& transform): source(source), target(target), transform(transform) {
+
+    }
+
+    const std::string& get_source() const {
+        return source;
+    }
+
+    const std::string& get_target() const {
+        return target;
+    }
+
+    const Isometry& get_transform() const {
+        return transform;
+    }
+
+    /*
+        Directly multiplying the transform with a vector only works on transforming points. 
+        When transforming vectors that represents directions, use this function instead.
+        It's implemented using homogeneous coordinates.
+
+        TODO: Should this be placed in some kind of utility header? Should other nodes get access to this class definition?
+    */
+    const Eigen::Vector3d apply_to_direction(const Eigen::Vector3d& src_vec) {
+        const Eigen::Vector4d src_vec_hom(src_vec.x(), src_vec.y(), src_vec.z(), 0);
+        const Eigen::Vector4d trgt_vec_hom = transform.matrix() * src_vec_hom;
+        return Eigen::Vector3d(trgt_vec_hom.x(), trgt_vec_hom.y(), trgt_vec_hom.z());
+    }
+};
+
+std::unordered_map<TransformKey, TransformObj> transforms;
+
+void add_transform(const std::string& source, const std::string& target, const Isometry& transform) {
+    TransformKey k;
+    k.frame1 = source;
+    k.frame2 = target;
+    transforms[k] = TransformObj(source, target, transform);
+}
 
 void TransformsNode::run(const nlohmann::json& params) { 
-    TransformKey k;
-    k.frame1 = "base";
-    k.frame2 = "arm";
-    transforms[k] = Eigen::Isometry3d::Identity();
+    add_transform("base", "arm", Isometry::Identity());
 
     TransformKey kk;
     kk.frame1 = "arm";
     kk.frame2 = "base";
     auto it = transforms.find(kk);
     if (it != transforms.end()) {
-        spdlog::info("FOUND IT!");
+        const Isometry t = it->second.get_transform();
+        std::stringstream ss;
+        ss << t.matrix();
+        spdlog::info("FOUND IT!\n{}", ss.str());
     } else {
         spdlog::warn("SOMETHING IS BONKERS");
     }
